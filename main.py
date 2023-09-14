8c60d2ecc49eedcd97872db8c7bb9934f868c03b
Comben <CombenD@sbhs.nsw.edu.au>
2023-09-14 15:00:25 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from djitellopy import Tello
+
+tello = Tello()
+tello.connect()
+
+tello.enable_mission_pads()
+tello.set_mission_pad_detection_direction(1)  # forward detection only  只识别前方
+
+tello.takeoff()
+
+pad = tello.get_mission_pad_id()
+
+tello.go_xyz_speed_yaw_mid(225, 0, 100, 20, 0, 1, 2)
+
+tello.disable_mission_pads()
+tello.land()
+tello.end()
